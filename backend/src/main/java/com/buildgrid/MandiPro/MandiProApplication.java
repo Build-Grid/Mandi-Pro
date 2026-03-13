52e0b9226aff282e7a32633649d553ca4e478927
SAMEER GUPTA <sameerg@nrifintech.com>
2026-03-13 15:01:00 +0530

    [PROJECT] : SHREE GANESH.

---
+++ backend/src/main/java/com/buildgrid/MandiPro/MandiProApplication.java
@@ -0,0 +1,13 @@
+package com.buildgrid.MandiPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandiProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandiProApplication.class, args);
+	}
+
+}
